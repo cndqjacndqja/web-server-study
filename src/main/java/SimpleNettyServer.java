a3d398ef6d813970419506a4a190ac20f244cf15
cndqjacndqja <cndqjacndqja@gmail.com>
2024-09-07 23:54:57 +0900

    설명 추가

--- src/main/java/SimpleNettyServer.java
+++ src/main/java/SimpleNettyServer.java
@@ -17,14 +17,14 @@ public class SimpleNettyServer {
     }
 
     public void run() throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1); // 이벤트 그룹 1개, 이벤트 루프 1개, 쓰레드 1개, selector 1개
         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup)
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<SocketChannel>() {
+                .channel(NioServerSocketChannel.class) // bossGroup은 NioServerSocketChannel을 사용 -> TCP 요청 받아서 workerGroup에 넘기는 것까지는 NioServerSocketChannel이 담당. 이 과정에서 굳이 여러 개의 스레드가 필요하지 않아서 1개로 지정
+                .childHandler(new ChannelInitializer<SocketChannel>() { // workerGroup은 SocketChannel을 사용 -> TCP 요청을 받아서 처리하는 것까지는 SocketChannel이 담당. 이 과정에서 여러 개의 스레드가 필요하므로 workerGroup은 여러 개로 지정.
                     @Override
                     public void initChannel(SocketChannel ch) {
                         ch.pipeline().addLast(new HttpRequestDecoder());
